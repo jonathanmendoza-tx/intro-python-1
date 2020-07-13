f98d2574aa46413143b7f8f6932a66d2cc86ed9d
Jonathan Mendoza <jon_94us@yahoo.com>
2020-07-12 22:01:39 -0500

    updating homework for grading

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,27 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+print('Command line arguments:')
+for arg in sys.argv:
+	print(arg + '\n')
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+print(f'Platform:\n{sys.platform}\n')
+
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+py_ver = sys.version_info
 
+print(f'Python version:\n{py_ver[0]}.{py_ver[1]}.{py_ver[2]}\n')
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+print(f'Current proccess ID:\n{os.getpid()}\n')
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print(f'Current working dir:\n{os.getcwd()}\n')
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print(f'Current login:\n{os.getlogin()}\n')
